aca8653196c7d097727d104b339955c6e4fed4ec
it.consulting.om@gmail.com <it.consulting.om@gmail.com>
2019-12-05 23:09:50 +0530

    Added new validations

--- App/Containers/Register/RegisterScreen.js
+++ App/Containers/Register/RegisterScreen.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { Text, View,Picker, ScrollView, ActivityIndicator, Image,TouchableOpacity } from 'react-native'
+import { View,Picker, ScrollView, ActivityIndicator, Image,TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { PropTypes } from 'prop-types'
 import RegisterActions from 'App/Stores/Register/Actions'
 import Style from './RegisterScreenStyle'
 import Background from '../../Components/Background';
 import Logo from '../../Components/Logo';
-import Header from '../../Components/Header';
-import Button from '../../Components/Button';
 import TextInput from '../../Components/TextInput';
 import { nameValidator,genderValidator,dobValidator,emailValidator,bloodGroupValidator,frequentDonorValidator,countryValidator,stateValidator,districtValidator,talukValidator,placeValidator,areaValidator,addressValidator,pinCodeValidator } from '../../Core/utils';
-import { Container } from 'native-base';
+import { Container, Header, Content, Form, Item, Input, Left, Body, Right, Title,Icon, Label,Text,Button } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 
 /**
@@ -151,6 +149,14 @@ class RegisterScreen extends React.Component {
   }
   
   _onRegisterPressed = () => {
+    console.log('this.state.emailId',this.state.emailId);
+    const emailIdError = emailValidator(this.state.emailId);
+    if (emailIdError) {
+      this.setState({
+        emailIdError : emailIdError 
+      });
+      return;
+    }
     this.props.navigation.navigate('MainScreen');
   };
 
@@ -161,240 +167,31 @@ class RegisterScreen extends React.Component {
   render() {
     return (
       <Container>
-      <Header />
-      <Grid>
-          <Row>
-            <Col size={4}>
-            <Header>Registration Screen</Header>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-                <Text style={Style.label}>Name *</Text>
-            </Col>
-            <Col size={3}>
-                <TextInput
-                returnKeyType="next"
-                value={this.state.name}
-                onChangeText={this.handleNameChange}
-                error={!!this.state.nameError}
-                errorText={this.state.nameError}
-                autoCapitalize="none"
-                autoCompleteType="off"
-                textContentType="name"
-                keyboardType="default"
+        <Header>
+          <Body>
+            <Title>New User Registration</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Form>
+            <Item stackedLabel>
+              <Label>Name</Label>
+              <Input/>
+            </Item>
+            <Text>Username is mandatory</Text>
+            <Item stackedLabel>
+              <Label>Email ID</Label>
+              <Input
+                value={this.state.emailId}
+                onChangeText={this.handleEmailIdChange}
               />
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-              <Text style={Style.label}>Gender *</Text>
-            </Col>
-            <Col size={3}>
-               <Picker
-                selectedValue={this.state.gender}
-                style={Style.selectBox}
-                onValueChange={this.handleGenderChange}
-                itemStyle={{ backgroundColor: "grey", color: "blue", fontFamily:"Ebrima", fontSize:17 }}
-                >
-                <Picker.Item label="Male" value="male" />
-                <Picker.Item label="Female" value="female" />
-              </Picker>
-            </Col>
-          </Row>
-           <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Date Of Birth *</Text>
-            </Col>
-            <Col size={3}>
-            <TextInput
-        returnKeyType="next"
-        value={this.state.dob}
-        onChangeText={this.handleDOBChange}
-        error={!!this.state.dobError}
-        errorText={this.state.dobError}
-        autoCapitalize="none"
-        autoCompleteType="off"
-        textContentType="none"
-        keyboardType="default"
-      />
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Email ID</Text>
-            </Col>
-            <Col size={3}>
-            <TextInput
-        returnKeyType="next"
-        value={this.state.emailId}
-        onChangeText={this.handleEmailIdChange}
-        error={!!this.state.emailIdError}
-        errorText={this.state.emailIdError}
-        autoCapitalize="none"
-        autoCompleteType="off"
-        textContentType="none"
-        keyboardType="default"
-      />
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Blood Group *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.bloodGroup}
-        style={Style.selectBox}
-        onValueChange={this.handleBloodGroupChange}>
-        <Picker.Item label="O-Positive" value="O-Positive" />
-        <Picker.Item label="O-Negative" value="O-Negative" />
-      </Picker>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Frequent Donor *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.frequentDonor}
-        style={Style.selectBox}
-        onValueChange={this.handleFrequentDonorChange}>
-        <Picker.Item label="yes" value="Yes" />
-        <Picker.Item label="no" value="No" />
-      </Picker>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Country *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.country}
-        style={Style.selectBox}
-        onValueChange={this.handleCountryChange}>
-        <Picker.Item value="in" label="India" />
-        <Picker.Item value="sg" label="Singapore" />
-      </Picker>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>State *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.state}
-        style={Style.selectBox}
-        onValueChange={this.handleStateChange}>
-        <Picker.Item value="tn" label="Tamil Nadu" />
-        <Picker.Item value="kl" label="Kerala" />
-      </Picker>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>District *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.district}
-        style={Style.selectBox}
-        onValueChange={this.handleDistrictChange}>
-        <Picker.Item value="cp" label="ChengalPattu" />
-        <Picker.Item value="th" label="Thiruvallur" />
-      </Picker>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Taluk *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.taluk}
-        style={Style.selectBox}
-        onValueChange={this.handleTalukChange}>
-        <Picker.Item value="t1" label="Taluk1" />
-        <Picker.Item value="t2" label="Taluk2" />
-      </Picker>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Place *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.place}
-        style={Style.selectBox}
-        onValueChange={this.handlePlaceChange}>
-        <Picker.Item value="place1" label="Place1" />
-        <Picker.Item value="place2" label="Place2" />
-      </Picker>
-            </Col>
-          </Row>    
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Area *</Text>
-            </Col>
-            <Col size={3}>
-            <Picker
-        selectedValue={this.state.area}
-        style={Style.selectBox}
-        onValueChange={this.handleAreaChange}>
-        <Picker.Item value="area1" label="Area1" />
-        <Picker.Item value="area2" label="Area2" />
-      </Picker>
-            </Col>
-          </Row> 
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Address*</Text>
-            </Col>
-            <Col size={3}>
-            <TextInput
-        returnKeyType="next"
-        value={this.state.address}
-        onChangeText={this.handleAddressChange}
-        error={!!this.state.addressError}
-        errorText={this.state.addressError}
-        autoCapitalize="none"
-        autoCompleteType="off"
-        textContentType="none"
-        keyboardType="default"
-      />
-            </Col>
-          </Row> 
-          <Row>
-            <Col size={1}>
-            <Text style={Style.label}>Pin code*</Text>
-            </Col>
-            <Col size={3}>
-            <TextInput
-        returnKeyType="next"
-        value={this.state.pinCode}
-        onChangeText={this.handlePinCodeChange}
-        error={!!this.state.pinCodeError}
-        errorText={this.state.pinCodeError}
-        autoCapitalize="none"
-        autoCompleteType="off"
-        textContentType="none"
-        keyboardType="default"
-      />
-            </Col>
-          </Row>      
-          <Row>
-            <Col size={4}>
-              <Button mode="contained" onPress={this._onRegisterPressed}>
-                  Register
-              </Button>
-            </Col>
-          </Row>
-      </Grid>
-      </Container>  
+            </Item>
+            <Text>{(this.state.emailIdError)?"Email Id is mandatory":""}</Text>
+            <Button primary onPress={this._onRegisterPressed}><Text> Register </Text></Button>
+          </Form>
+        </Content>
+      </Container>
     )
   }
   
